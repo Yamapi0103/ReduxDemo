0ccd56dbd3a67606d85a5289d1e8cd2620842bfd
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-09-24 16:49:04 +0800

    建store,reducer

--- demo1/src/TodoList.js
+++ demo1/src/TodoList.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react'; // 快捷鍵:imrc
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
+import store from './store'
 
-
-const data = [
-  '早8點開晨會，分配今天的代碼任務',
-  '早9點和項目經理開需求溝通會',
-  '早9點和項目經理開需求溝通會'
-]
 class TodoList extends Component {  // 快捷鍵:ccc
+
+  constructor(props) {
+    super(props)
+    console.log(store.getState())
+    this.state = store.getState()
+  }
 
   render () {
     return (
       <div>
         <div style={{ margin: '10px' }}>
-          <Input placeholder="Write Something" style={{ width: '250px', marginRight: '10px' }} />
+          <Input placeholder={this.state.inputValue} style={{ width: '250px', marginRight: '10px' }} />
           <Button type="primary">增加</Button>
         </div>
         <div style={{ margin: '10px', width: '300px' }}>
           <List
             bordered
-            dataSource={data}
+            dataSource={this.state.list}
             renderItem={item => (<List.Item>{item}</List.Item>)}
           />
         </div>
---
+++ demo1/src/store/reducer.js
@@ -0,0 +1,11 @@
+const defaultState = {
+  inputValue:'Write Something',
+  list : [
+    '早8點開晨會，分配今天的代碼任務',
+    '早9點和項目經理開需求溝通會',
+    '早9點和項目經理開需求溝通會'
+  ]
+}
+export default (state = defaultState, action) => {
+  return state
+}
