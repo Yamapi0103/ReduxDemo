09696af498322f56eac4e88df83dc6665fc52cd9
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-09-24 16:32:01 +0800

    新增antd的Input、Button、List

--- demo1/src/TodoList.js
+++ demo1/src/TodoList.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'; // 快捷鍵:imrc
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
 
+
+const data = [
+  '早8點開晨會，分配今天的代碼任務',
+  '早9點和項目經理開需求溝通會',
+  '早9點和項目經理開需求溝通會'
+]
 class TodoList extends Component {  // 快捷鍵:ccc
 
-  render() { 
+  render () {
     return (
-      <div>Hello World</div>
+      <div>
+        <div style={{ margin: '10px' }}>
+          <Input placeholder="Write Something" style={{ width: '250px', marginRight: '10px' }} />
+          <Button type="primary">增加</Button>
+        </div>
+        <div style={{ margin: '10px', width: '300px' }}>
+          <List
+            bordered
+            dataSource={data}
+            renderItem={item => (<List.Item>{item}</List.Item>)}
+          />
+        </div>
+      </div>
     );
   }
 }
- 
+
 export default TodoList;
