ad5d29c04604754712f9481f9e20b1fbb7e618bb
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-09-24 15:57:24 +0800

    install ant design

---
+++ demo1/src/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'; // 快捷鍵:imrc
+
+class TodoList extends Component {  // 快捷鍵:ccc
+
+  render() { 
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+ 
+export default TodoList;
