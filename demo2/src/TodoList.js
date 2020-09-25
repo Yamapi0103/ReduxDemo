3f01d9c1d9ea99a6b2ece7929332f4014593100f
Yamapi0103 <38974693+Yamapi0103@users.noreply.github.com>
2020-09-26 03:32:11 +0800

    react_redux 優化

--- demo2/src/TodoList.js
+++ demo2/src/TodoList.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux'
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render () {
+
+  const TodoList = (props) => {
+    let { inputValue, inputChange, clickBtn, list } = props
     return (
       <div>
         <div>
-          <input value={this.props.inputValue} onChange={this.props.inputChange} />
-          <button onClick={this.props.clickBtn}>提交</button>
+          <input value={inputValue} onChange={inputChange} />
+          <button onClick={clickBtn}>提交</button>
         </div>
         <ul>
           {
-            this.props.list.map((item,index)=>{
+            list.map((item, index) => {
               return (<li key={index}>{item}</li>)
             })
           }
@@ -22,12 +20,10 @@ class TodoList extends Component {
     );
   }
 
-}
-
 const stateToProps = (state) => {
   return {
     inputValue: state.inputValue,
-    list:state.list
+    list: state.list
   }
 }
 const dispatchToProps = (dispatch) => {
@@ -39,8 +35,8 @@ const dispatchToProps = (dispatch) => {
       }
       dispatch(action)
     },
-    clickBtn(){
-      let action = {type:'add_item'}
+    clickBtn () {
+      let action = { type: 'add_item' }
       dispatch(action)
     }
   }
